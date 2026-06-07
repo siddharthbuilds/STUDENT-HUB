6fb80f717a5fdbeffb65275aa62a5e1dd8016f0b
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-07 08:58:57 +0530

    created register page

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -1,16 +1,24 @@
 import { Input } from "../Components/login/Input"
 import { Password } from "../Components/login/Password"
 import {ButtonLogin} from "../Components/login/ButtonLogin"
+import { Headerbox } from "../Components/login/Headerbox"
+import { useState } from "react"
 import "./RegisterPage.css"
 export function RegisterPage()
 {
+    const [capsCheck, setCapsCheck] = useState(false);
     return(
         <>
+        <Headerbox />
         <div className="div-register">
-            <Input placeholder="Enter Your Name" type="text"/>
-            <Input placeholder="Enter Your Account ID" type="text" />
-            <Input placeholder="Enter Your Email ID" type="email"/>
-            <Password />
+            
+            <div className="div-register-txt1">
+                Register Now!
+            </div>
+            <Input placeholder="Enter Your Name" type="text" setCapsCheck={setCapsCheck}/>
+            <Input placeholder="Enter Your Account ID" type="text" setCapsCheck={setCapsCheck} />
+            <Input placeholder="Enter Your Email ID" type="email" setCapsCheck={setCapsCheck}/>
+            <Password capsCheck={capsCheck} setCapsCheck={setCapsCheck}/>
             <ButtonLogin text="Register" />
         </div>
         
