7f20b9907126d3b5381fb9c63b0e53edc80828ab
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-26 22:53:53 +0530

    added react states to track changes

--- frontend/src/Components/addsem/CourseBox.jsx
+++ frontend/src/Components/addsem/CourseBox.jsx
@@ -1,17 +1,40 @@
 import "./CourseBox.css"
 import { Input } from "../login/Input.jsx"
 import {ButtonLogin} from "../login/ButtonLogin.jsx"
-export function CourseBox()
+import { useState } from "react"
+export function CourseBox({toggleShowCourseBox, courseList, setCourseList})
 {
+    const [courseName, setCourseName] = useState('');
+    const [courseCredits, setCourseCredits]= useState('');
+
+    function trackCourseName(event)
+    {
+        setCourseName(event.target.value);
+    }
+
+    function trackCourseCredits(event)
+    {
+        setCourseCredits(event.target.value);
+    }
+
+
+    function addCourseBtn()
+    {
+        const obj = {courseName,courseCredits};
+        const newList = [...courseList,obj];
+        setCourseList(newList);
+        toggleShowCourseBox();
+    }
+
     return(
         <>
         <div className="div-course-box">
             <div className="div-course-attributes">
-                <Input placeholder="Course Name"/>
-                <Input placeholder="Credits" size="3"/>
+                <Input placeholder="Course Name" size="40" onChange={trackCourseName}/>
+                <Input placeholder="Credits" size="3" onChange={trackCourseCredits}/>
             </div>
             <div>
-                <ButtonLogin text="Add" />
+                <ButtonLogin text="Add" onClick={addCourseBtn}/>
             </div>
         </div>
         </>
