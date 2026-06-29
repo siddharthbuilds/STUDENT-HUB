16174646e7a7694c613e5884360b2d4a51122462
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-29 11:15:21 +0530

    created schedule related components

---
+++ frontend/src/Components/addsem/ScheduleAdd.jsx
@@ -0,0 +1,11 @@
+import "./ScheduleAdd.css"
+import { ButtonAddCourse } from "./ButtonAddCourse"
+
+export function ScheduleAdd()
+{
+    return(
+        <>
+        <ButtonAddCourse text="+ Add Hours" />
+        </>
+    )
+}
---
+++ frontend/src/Components/addsem/ScheduleDay.jsx
@@ -0,0 +1,26 @@
+import "./ScheduleDay.css"
+import { ScheduleAdd } from "./ScheduleAdd";
+export function ScheduleDay()
+{
+    const dayList=["Mon","Tue","Wed","Thu","Fri"];
+    return(
+        <>
+            <div className="div-schedule-day-header">
+                Add your Schedule
+            </div>
+            <div className="div-schedule-day">
+               {dayList.map(day=>{
+                return(
+                    <div key={day}>
+                        <button className="btn-schedule-day">
+                            {day}
+                        </button>
+                    </div>
+                )
+            })}
+            </div>
+            <ScheduleAdd />
+            
+        </>
+    )
+}
