0c6f3c577d3315d4cd0f940b2cc2612d9dcba4c3
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-07-02 12:53:12 +0530

    added dates utils

---
+++ frontend/src/Utils/Dates.js
@@ -0,0 +1,36 @@
+export default function getSatDates (Date1, Date2)
+{
+    const [startDay,startMonth,startYear] = Date1.split('-').map(Number);
+    const startDate = new Date (startYear,startMonth-1,startDay);
+
+    const [endDay,endMonth,endYear] = Date2.split('-').map(Number);
+    const endDate = new Date (endYear,endMonth-1,endDay);
+
+    let saturdayList = [];
+
+    let currentDate = startDate;
+
+    while(true)
+    {
+        let day = currentDate.getDay();
+        if (day==6)
+        {
+            break;
+        }
+        else{
+            currentDate.setDate(currentDate.getDate()+1);
+        }
+    }
+
+    while (currentDate <= endDate) {
+    saturdayList.push(new Date(currentDate)); // Store a copy of the date
+    currentDate.setDate(currentDate.getDate() + 7);
+  }
+
+     return saturdayList.map(dayObj => {
+    const y = dayObj.getFullYear();
+    const m = String(dayObj.getMonth() + 1).padStart(2, '0');
+    const d = String(dayObj.getDate()).padStart(2, '0');
+    return `${d}-${m}-${y}`;
+  });
+}
