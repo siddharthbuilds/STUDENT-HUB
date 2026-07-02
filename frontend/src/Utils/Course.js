c2a1450511bac6faac2e5093d8480165f2f13ab6
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-07-02 20:43:41 +0530

    modified course js for multiple functionalities

--- frontend/src/Utils/Course.js
+++ frontend/src/Utils/Course.js
@@ -1,7 +1,10 @@
-export default function getObj(name,credits)
+export default function getObj(arg1,arg2,arg3)
 {
-    return ({courseName: name,
-            courseCredits: credits,
+    if(arguments.length==2)return ({courseName: arg1,
+            courseCredits: arg2,
             mon:0,tue:0,wed:0,thu:0,fri:0
     })
+    else{
+        return ({from:arg1,to:arg2,des:arg3})
+    }
 }
