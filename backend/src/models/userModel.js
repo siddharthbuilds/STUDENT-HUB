6ba9456067397097600250e11471fabca8af671c
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-07-18 20:36:47 +0530

    moved password hasing to userModel

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,14 +1,15 @@
 import mydb from "../config/database.js"
-
+import bcrypt from "bcrypt"
 class User{
     static async existsId(id){
-        const query = 'SELECT EXISTS (SELECT 1 FROM users WHERE userId=?) AS row_exists;'
+        const query = 'SELECT EXISTS (SELECT 1 FROM users WHERE user_id=?) AS row_exists;'
         const [rows] = await mydb.query(query,[id])
         const exist = rows[0].row_exists===1;
         return exist;
     }
 
     static async existsEmail(email){
+        email = email.replaceAll(" ","").toLowerCase();
         const query = 'SELECT EXISTS (SELECT 1 FROM users WHERE email=?) AS row_exists;'
         const [rows] = await mydb.query(query,[email])
         const exist = rows[0].row_exists===1;
@@ -17,10 +18,13 @@ class User{
 
     static async createUser({userId,userName,password,email})
     {
+        userId = userId.replaceAll(" ","").toLowerCase();
+        const passwordHashed = await bcrypt.hash(password,10);
+        email = email.replaceAll(" ","").toLowerCase();
         try{
-            const addQuery = `INSERT INTO users 
+            const addQuery = `INSERT INTO users (user_id, user_name, password_hash, email) 
             VALUES (?, ?, ?, ?)`;
-            const params = [userId, userName, password, email];
+            const params = [userId, userName, passwordHashed, email];
             await mydb.query(addQuery,params)
         }
         catch(err)
@@ -28,6 +32,15 @@ class User{
             throw err;
         }
     }
-}
 
+    static async getSemesters(userId)
+    {
+        const getSemQuery = `SELECT * from semesters WHERE user_id=?`;
+        const params = userId;
+        const queryResult = await mydb.query(getSemQuery,params);
+        const semesters = queryResult[0];
+        console.log(semesters);
+        return semesters;
+    }
+}
 export default User
