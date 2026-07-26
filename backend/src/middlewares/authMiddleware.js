d0155c890d2fea9581911de2dda3c7c224a54598
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-07-26 18:28:34 +0530

    added authenticate jwt middleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,4 +1,7 @@
 import User from "../models/userModel.js"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config();
 export async function registerMiddleware(req,res,next)
 {
     if(!req.body.userId||!req.body.userName||!req.body.password||!req.body.email)
@@ -30,4 +33,27 @@ export async function loginMiddleware(req,res,next)
         return res.status(400).json({message: 'User ID not exists'});
     }
     next();
+}
+
+export async function authenticateToken(req,res,next)
+{
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+    try 
+    {
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded; //{userId, iat, exp}
+
+    next();
+    } 
+    catch (err) 
+    {
+        return res.status(401).json({
+            message: "Invalid or expired token"
+        });
+    }
 }
