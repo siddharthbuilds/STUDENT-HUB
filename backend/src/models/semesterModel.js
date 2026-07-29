3fc915172553e3bb6459a7fa4876568bc95dfe2f
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-07-29 19:37:55 +0530

    added addSchedule method

--- backend/src/models/semesterModel.js
+++ backend/src/models/semesterModel.js
@@ -32,6 +32,45 @@ class Semester{
         return courseMap;
     }
 
+    static async addSchedule(semId, courseMap)
+    {
+        const query =  `INSERT INTO schedules (sem_id, course_id, day, hour)
+                        VALUES ?`;
+        const dayMap = {
+            mon: 1,
+            tue: 2,
+            wed: 3,
+            thu: 4,
+            fri: 5
+        };
+
+        const dayHourCounter = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0
+        };
+
+        const rows = [];
+
+        for (const { courseId, course } of Object.values(courseMap))
+        {
+            for(const [dayName, dayNumber] of Object.entries(dayMap))
+            {
+                if(course[dayName] === 0) {continue;}
+                const multiplier = parseInt(course[dayName].split(' ')[1]);
+                for(let i=1;i<=multiplier;i++)
+                {
+                    rows.push([semId, courseId, dayNumber, ++dayHourCounter[dayNumber]]);
+                }
+                
+            }
+        }
+
+        rows && rows.length>0 && await mydb.query(query,[rows]);
+    }
+
 }
 
 export default Semester
