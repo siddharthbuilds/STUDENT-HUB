27d056a29a4ead2b9325a6f7bcec243ce834176b
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-08-01 10:20:25 +0530

    addes semester controller to semester routes

--- backend/src/routes/semesterRoutes.js
+++ backend/src/routes/semesterRoutes.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
+import { addSemesterController } from "../controllers/semesterController.js";
 const router = Router()
 
 router.use(authenticateToken);
 
-router.post('/add-semester')
+router.post('/add-semester',addSemesterController);
 export default router;
