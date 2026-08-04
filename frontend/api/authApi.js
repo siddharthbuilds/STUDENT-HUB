f63e07f78d518125ad1fabd6c4bea2f6ad7e1d96
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-08-04 18:37:15 +0530

    added authApi

--- frontend/api/authApi.js
+++ frontend/api/authApi.js
@@ -1,1 +1,12 @@
-import api from "./axios.js"
+import api from "./axios.js";
+const route = '/users';
+
+export function login(data)
+{
+    return api.post(`${route}/login`,data);
+}
+
+export function register(data)
+{
+     return api.post(`${route}/register`,data);
+}
