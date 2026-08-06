25baba465b84602ce11371a6528c3509a109cf15
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-08-06 19:05:56 +0530

    started with react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,22 @@ import { GradesPage } from "./Pages/GradesPage"
 import { HomePage } from "./Pages/HomePage"
 import { AddSemPage } from "./Pages/AddSemPage"
 import { PlanYourBunksPage } from "./Pages/PlanYourBunks"
+import {Routes, Route} from "react-router"
 function App() {
   
 
   return (
     <>
-    {/* <LoginPage />
-    <DashBoardPage />
-    <RegisterPage />
-    <AttendancePage />
-    <GradesPage />
-    <HomePage /> 
-    <AddSemPage /> */}
-    <PlanYourBunksPage />
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashBoardPage />} />
+      <Route path="/attendance" element={<AttendancePage />} />
+      <Route path="/semesters" element={<HomePage />} />
+      <Route path="/grades" element={<GradesPage />} />
+      <Route path="/add-semester" element={<AddSemPage />} />
+      <Route path="/planner" element={<PlanYourBunksPage />} />
+    </Routes>
     </>
   )
 }
