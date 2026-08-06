bc0df3dd3b6f636861c523808549344c5c989b47
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-08-06 19:44:48 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,17 @@
 import express from "express"
+import cors from "cors"
 import userRoutes from "./routes/userRoutes.js"
 import semesterRoutes from "./routes/semesterRoutes.js"
 import attendanceRoutes from "./routes/attendanceRoutes.js"
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 
 app.use('/',express.json());
 app.use('/api/users',userRoutes);
