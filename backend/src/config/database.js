801d3cfeffedf203055341cadca4723e02ed65ee
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-08-22 21:38:59 +0530

    getting things ready for deployment

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,10 +3,10 @@ import mysql from "mysql2/promise"
 dotenv.config()
 
 const pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
+    host:process.env.DB_host,
+    user:process.env.DB_user,
     password:process.env.DB_password,
-    database: "studenthub"
+    database: process.env.DB_database
 })
 
 export default pool
--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api"
+    baseURL:import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(config=>{
