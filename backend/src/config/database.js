161bf94ed1d04d7028253c649ef9b7780c5b02be
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-09-04 21:25:22 +0530

    fixed database configurations

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,9 +4,13 @@ dotenv.config()
 
 const pool = mysql.createPool({
     host:process.env.DB_host,
+    port: process.env.DB_port,
     user:process.env.DB_user,
     password:process.env.DB_password,
-    database: process.env.DB_database
+    database: process.env.DB_database,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export default pool
