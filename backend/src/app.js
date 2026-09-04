e142a16b562b77ce4581f5895670466cf52c2285
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-09-04 20:40:25 +0530

    fixed backend deployment issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,13 @@ import userRoutes from "./routes/userRoutes.js"
 import semesterRoutes from "./routes/semesterRoutes.js"
 import attendanceRoutes from "./routes/attendanceRoutes.js"
 import gradeRoutes from "./routes/gradeRoutes.js"
+import dotenv from "dotenv"
+
+dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization','x-sem-id']
 };
@@ -19,4 +22,8 @@ app.use('/api/users',userRoutes);
 app.use('/api/semesters',semesterRoutes);
 app.use('/api/attendance',attendanceRoutes);
 app.use('/api/grades',gradeRoutes);
-app.listen(3000);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
